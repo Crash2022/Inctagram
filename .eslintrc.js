c78400bd6ffe3639f73a7c41efd532b026294180
Lecser <107482432+Lecser@users.noreply.github.com>
2023-04-29 01:00:41 +0300

    add prettier & eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        'plugin:react/recommended',
+        'standard-with-typescript',
+        'next/core-web-vitals',
+        'plugin:react-hooks/recommended',
+        'prettier'
+    ],
+    overrides: [],
+    parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    plugins: ['react'],
+    rules: {
+        '@typescript-eslint/consistent-type-imports': 'off',
+        '@typescript-eslint/restrict-template-expressions': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off'
+    }
+};
