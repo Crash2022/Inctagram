fe4df0a88c1bff8fced6db7642fce1a532871617
Crash2022 <crash555@mail.ru>
2023-04-29 13:36:31 +0500

    fix: img issue v2

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,30 +5,30 @@ export const Footer = () => {
     return (
         <footer className={s.footer}>
             <div className={s.container}>
-                <div>
-                    <Image
-                        src={'./assets/icons/home-icon.svg'}
-                        alt='home-icon'
-                        width={24}
-                        height={24}
-                    />
-                </div>
-                <div>
-                    <Image
-                        src={'./assets/icons/add-icon.svg'}
-                        alt='add-icon'
-                        width={24}
-                        height={24}
-                    />
-                </div>
-                <div>
-                    <Image
-                        src={'./assets/icons/profile-icon.svg'}
-                        alt='profile-icon'
-                        width={24}
-                        height={24}
-                    />
-                </div>
+                {/*<div>*/}
+                {/*    <Image*/}
+                {/*        src={'./assets/icons/home-icon.svg'}*/}
+                {/*        alt='home-icon'*/}
+                {/*        width={24}*/}
+                {/*        height={24}*/}
+                {/*    />*/}
+                {/*</div>*/}
+                {/*<div>*/}
+                {/*    <Image*/}
+                {/*        src={'./assets/icons/add-icon.svg'}*/}
+                {/*        alt='add-icon'*/}
+                {/*        width={24}*/}
+                {/*        height={24}*/}
+                {/*    />*/}
+                {/*</div>*/}
+                {/*<div>*/}
+                {/*    <Image*/}
+                {/*        src={'./assets/icons/profile-icon.svg'}*/}
+                {/*        alt='profile-icon'*/}
+                {/*        width={24}*/}
+                {/*        height={24}*/}
+                {/*    />*/}
+                {/*</div>*/}
             </div>
         </footer>
     );
