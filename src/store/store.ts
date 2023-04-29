5f4f5dcd7a8b528973151d5f3fa952811ee38469
Crash2022 <crash555@mail.ru>
2023-04-29 15:34:53 +0500

    add: features example

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,3 +1,47 @@
-// RTK store
+import { AnyAction, combineReducers, configureStore } from '@reduxjs/toolkit';
+import { createWrapper, HYDRATE } from 'next-redux-wrapper';
+import authReducer from '@/redux/slices/auth/authSlice';
+import appReducer from '@/redux/slices/app/appSlice';
+import moderationSlice from '@/redux/slices/moderation/moderationSlice';
+import s_adminSlice from '@/redux/slices/s_admin/s_adminSlice';
+import adminSlice from '@/redux/slices/admin/adminSlice';
 
-export default {}
+const combinedReducer = combineReducers({
+    app: appReducer,
+    auth: authReducer,
+    s_admin: s_adminSlice,
+    admin: adminSlice,
+    moderation: moderationSlice
+});
+
+const reducer: typeof combinedReducer = (state, action: AnyAction) => {
+    switch (action.type) {
+        case HYDRATE:
+            return { ...state, ...action.payload };
+
+        default:
+            return combinedReducer(state, action);
+    }
+};
+
+export const makeStore = () => configureStore({ reducer });
+
+type Store = ReturnType<typeof makeStore>;
+
+export type AppDispatch = Store['dispatch'];
+export type RootState = ReturnType<Store['getState']>;
+
+export type AsyncThunkType<RV = unknown> = {
+    state: RootState;
+    dispatch: AppDispatch;
+    rejectValue?: RV;
+    serializedErrorType?: unknown;
+    pendingMeta?: unknown;
+    fulfilledMeta?: unknown;
+    rejectedMeta?: unknown;
+    extra?: { s: string; n: number };
+};
+
+export const wrapper = createWrapper(makeStore /*{ debug: true }*/);
+
+// export default {}
