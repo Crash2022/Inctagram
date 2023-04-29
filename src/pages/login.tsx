ef4431b32aea8fa797a655488c67ea6c63af1bdd
Crash2022 <crash555@mail.ru>
2023-04-29 12:10:49 +0500

    test: eslint v2

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,21 +1,20 @@
-import React, {useEffect} from 'react'
-import Head from 'next/head'
-import s from '../styles/Login.module.scss'
-import {useSnackbar} from 'notistack'
+import React, { useEffect } from 'react';
+import Head from 'next/head';
+import s from '../styles/Login.module.scss';
+import { useSnackbar } from 'notistack';
 
 const Login = () => {
-
-    const {enqueueSnackbar} = useSnackbar()
+    const { enqueueSnackbar } = useSnackbar();
 
     useEffect(() => {
-        enqueueSnackbar('Проверка снекбара', {variant: 'info', autoHideDuration: 2000})
-    }, [])
+        enqueueSnackbar('Проверка снекбара', { variant: 'info', autoHideDuration: 2000 });
+    }, []);
 
     return (
         <>
             <Head>
                 <title>Inctagram Login</title>
-                <meta name='title' content='Login'/>
+                <meta name='title' content='Login' />
             </Head>
             <div className={s.loginPage}>
                 <div className={s.container}>
