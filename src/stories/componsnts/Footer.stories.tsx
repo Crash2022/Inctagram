3647add8b8b8747498b44ac6356c0e6d8ae0765f
OlegPolishchuk <oleg.makkorm@gmail.com>
2023-04-30 22:31:11 +0300

    add: loader story

--- src/stories/componsnts/Footer.stories.tsx
+++ src/stories/componsnts/Footer.stories.tsx
@@ -4,6 +4,14 @@ import {Footer} from "../../components/Footer/Footer";
 const meta: Meta<typeof Footer> = {
   title: 'Components/Footer',
   component: Footer,
+  tags: ['aotodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: 'It show only if @media(max-width: 390px)'
+      }
+    }
+  }
 }
 
 export default meta;
