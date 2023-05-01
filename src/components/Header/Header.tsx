2d4335f94d3c62682c7bc28f2b9f69d8284b3aed
Crash2022 <crash555@mail.ru>
2023-05-01 13:18:58 +0500

    refactor: Header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,30 @@
 import cls from './Header.module.scss';
+import Image from 'next/image';
+import LogoIcon from '../../../public/assets/images/logo.png'
 
 export const Header = () => {
+
     return (
         <header className={cls.headerWrapper}>
             <div className={cls.header}>
-                <span>Inctagram</span>
+                <div className={cls.header_logo}>
+                    <div className={cls.logo}>
+                        I
+
+                        {/*<img src={LogoIcon} alt="logo-icon"/>*/}
+
+                        {/*<Image*/}
+                        {/*    src={'/../../public/assets/images/logo.png'}*/}
+                        {/*    alt={'home-icon'}*/}
+                        {/*    width={24}*/}
+                        {/*    height={24}*/}
+                        {/*/>*/}
+                    </div>
+                    <div className={cls.title}>Inctagram</div>
+                </div>
+                <div className={cls.auth}>
+                    AuthBlock
+                </div>
             </div>
         </header>
     );
