0105be977121771c1448d1b07a33d62865debb5d
Crash2022 <crash555@mail.ru>
2023-05-03 12:31:33 +0500

    fix: prettier semicolon false

--- src/common/api/auth.api.ts
+++ src/common/api/auth.api.ts
@@ -1,35 +1,36 @@
-import { instance } from '@/common/api/api.instance';
+import { instance } from '@/common/api/api.instance'
 
 export const authAPI = {
     register: async (arg: RegistrationParamsType) => {
-        return await instance.post<ResponseType>('auth/registration', arg);
+        return await instance.post<ResponseType>('auth/registration', arg)
     },
     login: async (arg: LoginParamsType) => {
-        return await instance.post<ResponseType>('auth/login', arg);
+        return await instance.post<ResponseType>('auth/login', arg)
     },
     recoverPass: async (arg: PassRecoveryType) => {
         return await instance.post<ResponseType>(
             'auth/password-recovery',
             { arg },
             { withCredentials: true }
-        );
+        )
     },
     logout: async () => {
-        return await instance.post<ResponseType>('auth/logout');
+        return await instance.post<ResponseType>('auth/logout')
     },
     me: async () => {
-        return await instance.get<ResponseType>('auth/me');
+        return await instance.get<ResponseType>('auth/me')
     }
-};
+}
 
 export interface RegistrationParamsType {
-    userName: string;
-    email: string;
+    userName: string
+    email: string
 }
 
 export interface LoginParamsType {
-    XXX: any;
+    XXX: any
 }
+
 export interface PassRecoveryType {
-    email: string;
+    email: string
 }
--- src/store/slices/auth/authSlice.ts
+++ src/store/slices/auth/authSlice.ts
@@ -1,19 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { authAPI, LoginParamsType, RegistrationParamsType } from '@/common/api/auth.api';
-import { createAppAsyncThunk } from '@/common/utils/createAppAsyncThunk';
-import { AxiosError } from 'axios';
+import { createSlice } from '@reduxjs/toolkit'
+import { authAPI, LoginParamsType, RegistrationParamsType } from '@/common/api/auth.api'
+import { createAppAsyncThunk } from '@/common/utils/createAppAsyncThunk'
+import { AxiosError } from 'axios'
 
 interface AuthStateType {
-    profile: any;
-    isLoggedIn: boolean;
-    isEmailSent: boolean;
+    profile: any
+    isLoggedIn: boolean
+    isEmailSent: boolean
 }
 
 const initialState: AuthStateType = {
     profile: null as any | null,
     isLoggedIn: false,
     isEmailSent: false
-};
+}
 
 const authSlice = createSlice<any, any>({
     name: 'auth',
@@ -22,59 +22,59 @@ const authSlice = createSlice<any, any>({
     extraReducers: (builder) => {
         builder
             .addCase(register.fulfilled, (state, action) => {
-                state.isLoggedIn = action.payload.isLoggedIn;
+                state.isLoggedIn = action.payload.isLoggedIn
             })
             .addCase(login.fulfilled, (state, action) => {
-                state.profile = action.payload.profile;
-                state.isLoggedIn = action.payload.isLoggedIn;
+                state.profile = action.payload.profile
+                state.isLoggedIn = action.payload.isLoggedIn
             })
             .addCase(logout.fulfilled, (state, action) => {
-                state.isLoggedIn = action.payload.isLoggedIn;
-                state.profile = null;
+                state.isLoggedIn = action.payload.isLoggedIn
+                state.profile = null
             })
             .addCase(recoverPass.fulfilled, (state, action) => {
-                state.isEmailSent = action.payload.isEmailSent;
-            });
+                state.isEmailSent = action.payload.isEmailSent
+            })
     }
-});
+})
 
 export const register = createAppAsyncThunk<{ isLoggedIn: boolean }, RegistrationParamsType>(
     'auth/register',
     async (data, thunkAPI) => {
         try {
-            await authAPI.register(data);
-            return { isLoggedIn: true };
+            await authAPI.register(data)
+            return { isLoggedIn: true }
         } catch (err: any | AxiosError) {
             return thunkAPI.rejectWithValue({
                 message: err.response.message,
                 fields: err.response.fields
-            });
+            })
         }
     }
-);
+)
 
 export const login = createAppAsyncThunk<{ profile: any; isLoggedIn: boolean }, LoginParamsType>(
     'auth/login',
     async (data, thunkAPI) => {
-        const res = await authAPI.login(data);
-        return { profile: res.data, isLoggedIn: true };
+        const res = await authAPI.login(data)
+        return { profile: res.data, isLoggedIn: true }
     }
-);
+)
 
 export const logout = createAppAsyncThunk<{ isLoggedIn: boolean }, any>(
     'auth/logout',
     async (_, thunkAPI) => {
-        await authAPI.logout();
-        return { isLoggedIn: false };
+        await authAPI.logout()
+        return { isLoggedIn: false }
     }
-);
+)
 
 export const recoverPass = createAppAsyncThunk<{ isEmailSent: boolean }, any>(
     'auth/recoverPass',
     async (data, thunkAPI) => {
-        await authAPI.recoverPass(data);
-        return { isEmailSent: true };
+        await authAPI.recoverPass(data)
+        return { isEmailSent: true }
     }
-);
+)
 
-export const authReducer = authSlice.reducer;
+export const authReducer = authSlice.reducer
