6f6fe67e185a155cb178698bf150f0f730b7bff0
Crash2022 <crash555@mail.ru>
2023-05-07 00:26:56 +0500

    add: CustomCheckbox in form

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,19 +1,19 @@
-import { type ButtonHTMLAttributes, memo, type PropsWithChildren } from 'react';
+import { type ButtonHTMLAttributes, memo, type PropsWithChildren } from 'react'
 
-import cls from './Button.module.scss';
-import clsx from 'clsx';
+import cls from './Button.module.scss'
+import clsx from 'clsx'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    className?: string;
-    theme?: 'primary' | 'primaryWhite' | 'outline' | 'clear';
+    className?: string
+    theme?: 'primary' | 'primaryWhite' | 'outline' | 'clear'
 }
 
 export const Button = memo((props: PropsWithChildren<ButtonProps>) => {
-    const { theme = 'primary', className, children, ...restProps } = props;
+    const { theme = 'primary', className, children, ...restProps } = props
 
     return (
         <button className={clsx(cls.button, [className, cls[theme]])} {...restProps}>
             {children}
         </button>
-    );
-});
+    )
+})
--- src/shared/ui/Checkbox/Checkbox.tsx
+++ src/shared/ui/Checkbox/Checkbox.tsx
@@ -1,24 +1,27 @@
-import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes} from 'react'
-import s from './CustomCheckbox.module.css'
+import React, { ChangeEvent, DetailedHTMLProps, InputHTMLAttributes } from 'react'
+import s from './Checkbox.module.css'
 
 // тип пропсов обычного инпута
-type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+type DefaultInputPropsType = DetailedHTMLProps<
+    InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+>
 
 type CheckboxPropsType = DefaultInputPropsType & {
     onChangeChecked?: (checked: boolean) => void
     spanClassName?: string
 }
 
-export const CustomCheckbox: React.FC<CheckboxPropsType> = (
-    {
-        type, // достаём и игнорируем, чтобы нельзя было задать другой тип инпута
-        onChange, onChangeChecked,
-        className, spanClassName,
-        children, // в эту переменную попадёт текст, типизировать не нужно, так как он затипизирован в React.FC
+export const Checkbox: React.FC<CheckboxPropsType> = ({
+    type, // достаём и игнорируем, чтобы нельзя было задать другой тип инпута
+    onChange,
+    onChangeChecked,
+    className,
+    spanClassName,
+    children, // в эту переменную попадёт текст, типизировать не нужно, так как он затипизирован в React.FC
 
-        ...restProps // все остальные пропсы попадут в объект restProps
-    }
-) => {
+    ...restProps // все остальные пропсы попадут в объект restProps
+}) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         onChange && onChange(e)
         onChangeChecked && onChangeChecked(e.currentTarget.checked)
