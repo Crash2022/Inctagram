589dc2fc3f8f1c8334e1732129ef64621b15ca40
Crash2022 <crash555@mail.ru>
2023-05-20 11:43:50 +0500

    add: Profile Management

--- src/components/Profile/Management/Management.tsx
+++ src/components/Profile/Management/Management.tsx
@@ -1,5 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import cls from './Management.module.scss'
+import { ContentBox } from '@/shared/ui/ContentBox/ContentBox'
+import { useTranslation } from 'next-i18next'
+import { RadioButton } from '@/shared/ui/RadioButton/RadioButton'
+
+type RadioAccountTypes = 'Personal' | 'Business'
+type RadioCostsTypes = '10' | '50' | '100'
 
 export const Management = () => {
-    return <div>Management</div>
+    const { t } = useTranslation('settings-management')
+
+    const radioAccountOptions = [t('Personal'), t('Business')]
+    const [radioAccTypeButton, setRadioAccTypeButton] = useState<RadioAccountTypes>('Personal')
+
+    const radioCostsOptions = [t('10PM'), t('50PM'), t('100PY')]
+    const [radioCostButton, setRadioCostButton] = useState<RadioCostsTypes>('10')
+
+    return (
+        <div className={cls.devices_mainBox}>
+            <div className={cls.account_types}>
+                <div className={cls.title}>{t('AccountType')}</div>
+                <ContentBox divClassName={cls.contentBox}>
+                    <RadioButton
+                        options={radioAccountOptions}
+                        value={radioAccTypeButton}
+                        onChangeOption={setRadioAccTypeButton}
+                    />
+                </ContentBox>
+            </div>
+            {radioAccTypeButton === 'Business' ? (
+                <div className={cls.account_types}>
+                    <div className={cls.title}>{t('Costs')}</div>
+                    <ContentBox divClassName={cls.contentBox}>
+                        <RadioButton
+                            options={radioCostsOptions}
+                            value={radioCostButton}
+                            onChangeOption={setRadioCostButton}
+                        />
+                    </ContentBox>
+                </div>
+            ) : (
+                ''
+            )}
+        </div>
+    )
 }
---
+++ src/shared/ui/RadioButton/RadioButton.tsx
@@ -0,0 +1,47 @@
+import React, { ChangeEvent, InputHTMLAttributes, DetailedHTMLProps } from 'react'
+import s from './RadioButton.module.css'
+
+type DefaultRadioPropsType = DetailedHTMLProps<
+    InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+>
+
+type RadioPropsType = DefaultRadioPropsType & {
+    options?: any[]
+    onChangeOption?: (option: any) => void
+}
+
+export const RadioButton: React.FC<RadioPropsType> = ({
+    type,
+    name,
+    options,
+    value,
+    onChange,
+    onChangeOption,
+    ...restProps
+}) => {
+    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange && onChange(e)
+        onChangeOption && onChangeOption(e.currentTarget.value)
+    }
+
+    // map options with key
+    const mappedOptions: any[] = options
+        ? options.map((option, i) => (
+              <label key={name + '-' + i} className={s.container}>
+                  <input
+                      type={'radio'}
+                      name={name}
+                      checked={option === value}
+                      value={option}
+                      onChange={onChangeCallback}
+                      {...restProps}
+                  />
+                  <span className={s.checkmark}></span>
+                  {option}
+              </label>
+          ))
+        : []
+
+    return <>{mappedOptions}</>
+}
