cda0f9a1ebaf6465c56967342ce05cb8bcd9ff0b
Crash2022 <crash555@mail.ru>
2023-05-20 12:26:28 +0500

    add: Profile Management message modal

--- src/components/Profile/Management/Management.tsx
+++ src/components/Profile/Management/Management.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import cls from './Management.module.scss'
 import { ContentBox } from '@/shared/ui/ContentBox/ContentBox'
 import { useTranslation } from 'next-i18next'
 import { RadioButton } from '@/shared/ui/RadioButton/RadioButton'
 import PaypalIcon from '../../../../public/assets/icons/pay-paypal.svg'
 import StripeIcon from '../../../../public/assets/icons/pay-stripe.svg'
+import { MessageModal } from '@/components/MessageModal/MessageModal'
 
 type RadioAccountTypes = 'Personal' | 'Business'
 type RadioCostsTypes = '10' | '50' | '100'
@@ -12,14 +13,35 @@ type RadioCostsTypes = '10' | '50' | '100'
 export const Management = () => {
     const { t } = useTranslation('settings-management')
 
+    const [open, setOpen] = useState<boolean>(false)
+
     const radioAccountOptions = [t('Personal'), t('Business')]
     const [radioAccTypeButton, setRadioAccTypeButton] = useState<RadioAccountTypes>('Personal')
 
     const radioCostsOptions = [t('10PM'), t('50PM'), t('100PY')]
     const [radioCostButton, setRadioCostButton] = useState<RadioCostsTypes>('10')
 
+    // const isError = true
+    // useEffect(() => {
+    //     if (isError) setOpen(true)
+    // }, [])
+
+    const messageModalOKHandler = () => {
+        setOpen(false)
+    }
+
     return (
         <div className={cls.devices_mainBox}>
+            <MessageModal
+                open={open}
+                setOpen={setOpen}
+                header={t('Error')}
+                text={t('Failed')}
+                buttonTitleOK={t('MainButton')}
+                extraCallbackOK={messageModalOKHandler}
+                longButton
+            />
+
             <div className={cls.account_types}>
                 <div className={cls.title}>{t('AccountType')}</div>
                 <ContentBox divClassName={cls.contentBox}>
