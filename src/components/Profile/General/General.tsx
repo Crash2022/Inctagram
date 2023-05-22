0b54fa3dbbc2b56133307a4efddd75d05bd224ff
Crash2022 <crash555@mail.ru>
2023-05-22 20:04:48 +0500

    add: delete avatar query

--- src/components/Profile/General/General.tsx
+++ src/components/Profile/General/General.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import cls from './General.module.scss'
 import ProfilePhoto from '../../../../public/assets/images/profile-photo.jpg'
 import DefaultProfileAvatar from '../../../../public/assets/images/default-avatar.png'
@@ -14,15 +14,31 @@ import { UpdateUserProfile } from '@/models/profile-types'
 import styles from '@/components/Forms/FormWrapper/Form.module.scss'
 import { Textarea } from '@/shared/ui/Textarea/Textarea'
 import { InputFile } from '@/shared/ui/InputFile/InputFile'
-import { useGetProfileDataQuery, useSetProfileDataMutation } from '@/services/UserProfileService'
+import {
+    useDeleteAvatarMutation,
+    useGetProfileDataQuery,
+    useSetProfileDataMutation,
+    useUploadAvatarMutation
+} from '@/services/UserProfileService'
 import { LoaderScreen } from '@/shared/ui/Loader/LoaderScreen'
+import { useSnackbar } from 'notistack'
 
 export const General = () => {
     const { t } = useTranslation('settings-general')
+    const { enqueueSnackbar } = useSnackbar()
+    const [avatar, setAvatar] = useState<string>('')
 
     const { data: profileData, isLoading } = useGetProfileDataQuery()
     const [setProfile, { data: setProfileData, isError: isSetError, isLoading: isSetLoading }] =
         useSetProfileDataMutation()
+    const [
+        uploadAvatar,
+        { data: uploadAvatarResult, error: uploadAvatarError, isLoading: uploadAvatarIsLoading }
+    ] = useUploadAvatarMutation()
+    const [
+        deleteAvatar,
+        { data: deleteAvatarResult, error: deleteAvatarError, isLoading: deleteAvatarIsLoading }
+    ] = useDeleteAvatarMutation()
 
     // const ProfileGeneralSchema = yup.object().shape({
     //     userName: yup.string().min(6, t('Err_Yup_Min')).max(30, t('Err_Yup_Max_Name')),
@@ -52,6 +68,46 @@ export const General = () => {
         console.log('profile response', res)
     }
 
+    const uploadAvatarHandler = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files.length) {
+            const file = e.target.files[0]
+
+            if (file.size < 7000000) {
+                convertFileToBase64(file, async (file64: string) => {
+                    setAvatar(file64)
+                    const avatarResponse = await uploadAvatar(file64)
+                })
+            } else {
+                console.error('Error: ', 'Файл слишком большого размера')
+                enqueueSnackbar('Файл слишком большого размера', {
+                    variant: 'warning',
+                    autoHideDuration: 3000
+                })
+            }
+
+            // if (file.size < 4000000) {
+            //     try {
+            //         const uploadResponse = await uploadPhoto(file)
+            //
+            //         console.log(uploadResponse.data)
+            //         const photoUrl = uploadResponse.data[0]?.url
+            //         if (photoUrl) setUploadedImage(photoUrl)
+            //     } catch (error) {
+            //         console.error('Error uploading file:', error)
+            //     }
+            // }
+        }
+    }
+
+    const convertFileToBase64 = (file: File, callBack: (value: string) => void) => {
+        const reader = new FileReader()
+        reader.onloadend = () => {
+            const file64 = reader.result as string
+            callBack(file64)
+        }
+        reader.readAsDataURL(file)
+    }
+
     useEffect(() => {
         if (profileData) {
             setValue('userName', profileData.userName)
@@ -61,30 +117,57 @@ export const General = () => {
             setValue('dateOfBirth', profileData.dateOfBirth)
             setValue('aboutMe', profileData.aboutMe)
         }
+
+        // if (profileData.avatars.length === 0) {
+        //     setAvatar(DefaultProfileAvatar)
+        // } else {
+        //     setAvatar(profileData.avatars[0].url)
+        // }
     }, [profileData])
 
     console.log('profileData', profileData)
 
     if (isLoading) return <LoaderScreen variant={'loader'} />
     if (isSetLoading) return <LoaderScreen variant={'loader'} />
+    if (uploadAvatarIsLoading) return <LoaderScreen variant={'loader'} />
+    if (deleteAvatarIsLoading) return <LoaderScreen variant={'loader'} />
 
     return (
         <form className={cls.profileSettings_general} onSubmit={handleSubmit(onSubmit)}>
             <div className={cls.general_mainBlock}>
                 <div className={cls.general_photoBlock}>
-                    <div className={cls.photo}>
-                        <Image
-                            src={DefaultProfileAvatar}
+                    <div className={cls.avatar}>
+                        <img
+                            src={
+                                profileData.avatars.length === 0
+                                    ? DefaultProfileAvatar
+                                    : profileData.avatars[0].url
+                            }
                             alt={'profile-avatar'}
                             width={204}
                             height={204}
                         />
-                        <div className={cls.add_photo}>
+                        {/*<Image*/}
+                        {/*    src={*/}
+                        {/*        profileData.avatars.length === 0*/}
+                        {/*            ? DefaultProfileAvatar*/}
+                        {/*            : profileData.avatars[0].url*/}
+                        {/*    }*/}
+                        {/*    alt={'profile-avatar'}*/}
+                        {/*    width={204}*/}
+                        {/*    height={204}*/}
+                        {/*/>*/}
+                        <div
+                            className={cls.delete_avatar}
+                            onClick={async () => {
+                                await deleteAvatar()
+                            }}
+                        >
                             <DeletePhotoIcon width={30} height={30} />
                         </div>
                     </div>
-                    <div className={cls.addPhoto_btn}>
-                        <InputFile title={t('AddPhoto')} />
+                    <div className={cls.addAvatar_btn}>
+                        <InputFile title={t('AddPhoto')} onChange={uploadAvatarHandler} />
                     </div>
                 </div>
                 <div className={cls.general_infoBlock}>
