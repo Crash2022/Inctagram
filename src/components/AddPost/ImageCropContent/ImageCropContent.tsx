68191f648aa63edc8bdf4dd38b9080509feb52f7
Crash2022 <crash555@mail.ru>
2023-05-30 14:12:37 +0500

    commit: first commit from new branch

--- src/components/AddPost/ImageCropContent/ImageCropContent.tsx
+++ src/components/AddPost/ImageCropContent/ImageCropContent.tsx
@@ -2,5 +2,5 @@ import React from 'react'
 import cls from './ImageCropContent.module.scss'
 
 export const ImageCropContent = () => {
-    return <div className={cls.imageCropModal_content}>ImageCropContent</div>
+    return <div className={cls.imageCropModal_content}>ImageCropContent666</div>
 }
