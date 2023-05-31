b0af3ce8ebfd77f654032d8b0747305af6b27a61
Crash2022 <crash555@mail.ru>
2023-05-31 12:51:22 +0500

    add: crop image to modal

--- src/components/AddPost/ImageCropContent/ImageCropContent.tsx
+++ src/components/AddPost/ImageCropContent/ImageCropContent.tsx
@@ -1,15 +1,47 @@
-import React from 'react'
-import cls from './ImageCropContent.module.scss'
-import Image from 'next/image'
+import React, { useCallback, useState } from 'react'
+import cls from './ImageCropContent.module.css'
+import Cropper from 'react-easy-crop'
 
 interface ImageCropContentProps {
     postPhoto: string
 }
 
 export const ImageCropContent = ({ postPhoto }: ImageCropContentProps) => {
+    const [crop, setCrop] = useState({ x: 0, y: 0 })
+    const [zoom, setZoom] = useState(1)
+    const [aspect, setAspect] = useState(3 / 2)
+
+    const onCropComplete = useCallback((croppedArea: any, croppedAreaPixels: any) => {
+        console.log(croppedArea, croppedAreaPixels)
+    }, [])
+
     return (
         <div className={cls.imageCropModal_content}>
-            <Image src={postPhoto} alt={'post-photo'} width={500} height={500} />
+            <div className={cls.crop_container}>
+                <Cropper
+                    image={postPhoto}
+                    crop={crop}
+                    zoom={zoom}
+                    aspect={aspect}
+                    onCropChange={setCrop}
+                    onCropComplete={onCropComplete}
+                    onZoomChange={setZoom}
+                />
+            </div>
+            <div className={cls.controls}>
+                <input
+                    className={cls.zoom_range}
+                    type={'range'}
+                    value={zoom}
+                    min={1}
+                    max={3}
+                    step={0.1}
+                    aria-labelledby={'Zoom'}
+                    onChange={(e) => {
+                        setZoom(Number(e.target.value))
+                    }}
+                />
+            </div>
         </div>
     )
 }
