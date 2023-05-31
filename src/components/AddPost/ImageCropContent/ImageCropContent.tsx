7f44796f3ee3973a208a83c331fc2e291cfb5af5
Crash2022 <crash555@mail.ru>
2023-05-31 13:18:25 +0500

    add: crop image with rotation

--- src/components/AddPost/ImageCropContent/ImageCropContent.tsx
+++ src/components/AddPost/ImageCropContent/ImageCropContent.tsx
@@ -1,14 +1,18 @@
 import React, { useCallback, useState } from 'react'
 import cls from './ImageCropContent.module.css'
 import Cropper from 'react-easy-crop'
+import { useTranslation } from 'react-i18next'
 
 interface ImageCropContentProps {
     postPhoto: string
 }
 
 export const ImageCropContent = ({ postPhoto }: ImageCropContentProps) => {
+    const { t } = useTranslation('add-post-modal')
+
     const [crop, setCrop] = useState({ x: 0, y: 0 })
-    const [zoom, setZoom] = useState(1)
+    const [zoom, setZoom] = useState<number>(1)
+    const [rotation, setRotation] = useState<number>(0)
     const [aspect, setAspect] = useState(3 / 2)
 
     const onCropComplete = useCallback((croppedArea: any, croppedAreaPixels: any) => {
@@ -22,25 +26,47 @@ export const ImageCropContent = ({ postPhoto }: ImageCropContentProps) => {
                     image={postPhoto}
                     crop={crop}
                     zoom={zoom}
+                    rotation={rotation}
                     aspect={aspect}
-                    onCropChange={setCrop}
                     onCropComplete={onCropComplete}
+                    onCropChange={setCrop}
                     onZoomChange={setZoom}
+                    onRotationChange={setRotation}
                 />
             </div>
             <div className={cls.controls}>
-                <input
-                    className={cls.zoom_range}
-                    type={'range'}
-                    value={zoom}
-                    min={1}
-                    max={3}
-                    step={0.1}
-                    aria-labelledby={'Zoom'}
-                    onChange={(e) => {
-                        setZoom(Number(e.target.value))
-                    }}
-                />
+                <div>{t('Zoom')}</div>
+                <div>
+                    <input
+                        className={cls.zoom_range}
+                        type={'range'}
+                        value={zoom}
+                        min={1}
+                        max={3}
+                        step={0.1}
+                        aria-labelledby={'Zoom'}
+                        onChange={(e) => {
+                            setZoom(Number(e.target.value))
+                        }}
+                    />
+                </div>
+            </div>
+            <div className={cls.controls} style={{ bottom: '40px' }}>
+                <div>{t('Rotation')}</div>
+                <div>
+                    <input
+                        className={cls.zoom_range}
+                        type={'range'}
+                        value={rotation}
+                        min={0}
+                        max={360}
+                        step={1}
+                        aria-labelledby={'Rotation'}
+                        onChange={(e) => {
+                            setRotation(Number(e.target.value))
+                        }}
+                    />
+                </div>
             </div>
         </div>
     )
