55f7f676953db44969d3c629dbd8876c1ed89e23
Crash2022 <crash555@mail.ru>
2023-06-07 16:02:37 +0500

    fix: recaptcha done!

--- src/components/Forms/ForgotPasswordForm/ForgotPassword.tsx
+++ src/components/Forms/ForgotPasswordForm/ForgotPassword.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import styles from '@/components/Forms/FormWrapper/Form.module.scss'
-// import { Input } from '@/shared/ui/Input/Input'
 // import { Checkbox } from '@/shared/ui/Checkbox/Checkbox'
 // import CaptchaIcon from 'public/assets/icons/reCaptcha.svg'
 import { Title } from '@/components/Forms/Title/Title'
@@ -20,26 +19,26 @@ import clsx from 'clsx'
 import { FormWrapper } from '@/components/Forms/FormWrapper/FormWrapper'
 import ReCAPTCHA from 'react-google-recaptcha'
 import { NEXT_PUBLIC_RECAPTCHA_SITE_KEY } from '@/shared/api/recaptcha-site-key'
+import { LoaderScreen } from '@/shared/ui/Loader/LoaderScreen'
 
 export const ForgotPasswordForm = () => {
     const { t } = useTranslation('forgot')
-    const recaptchaRef = React.createRef()
+    const recaptchaRef = useRef(null)
 
-    const [forgotPassword, { isError, isSuccess }] = useForgotPasswordMutation()
+    const [forgotPassword, { isError, isSuccess, isLoading }] = useForgotPasswordMutation()
 
     const [open, setOpen] = useState<boolean>(false)
     const [robot, setRobot] = useState<boolean>(false)
-    // const [email, setEmail] = useState<string>('')
-    // const [emailError, setEmailError] = useState<string>('')
 
     const ForgotSchema = yup.object().shape({
-        email: yup.string().required(t('Err_Yup_Required')).email(t('Err_Yup_Email')),
-        recaptcha: yup.boolean().oneOf([true], t('Robot'))
+        email: yup.string().required(t('Err_Yup_Required')).email(t('Err_Yup_Email'))
+        // recaptcha: yup.boolean().oneOf([true], t('Robot'))
     })
 
     const {
         control,
         handleSubmit,
+        setValue,
         formState: { errors }
     } = useForm<PasswordRecoveryType>({
         defaultValues: {
@@ -51,17 +50,17 @@ export const ForgotPasswordForm = () => {
     })
 
     const onSubmit: SubmitHandler<PasswordRecoveryType> = async (data: PasswordRecoveryType) => {
-        console.log('submit', data)
+        console.log('submit forgot', data)
         localStorage.setItem('email', control._getWatch('email'))
 
-        if (grecaptcha.getResponse() === '') {
-            // event.preventDefault()
+        if (recaptchaRef.current.getValue()) {
+            // console.log('submit recaptcha', control._getWatch('recaptcha'))
+            const forgotResponse = await forgotPassword(data)
+            console.log('forgot response', forgotResponse)
+            setOpen(true)
+        } else {
             setRobot(true)
-            // alert("Please click <I'm not a robot> before sending the job")
         }
-
-        // Execute the reCAPTCHA when the form is submitted
-        // recaptchaRef.current.execute()
 
         // if (!isSuccess) {
         //     const res = await forgotPassword(data)
@@ -69,50 +68,25 @@ export const ForgotPasswordForm = () => {
         //     setOpen(true)
         // }
         // if (isSuccess) {
-        //     data.recaptcha = true
+        //     // data.recaptcha = true
         //     const res = await forgotPassword(data)
         //     console.log('forgot response success', res)
         //     setOpen(true)
         // }
     }
 
-    // const handleSubmit = (event: any) => {
-    //     event.preventDefault()
-    //
-    //     // const trimValue = email.trim()
-    //     // if (trimValue && trimValue.length < 1) {
-    //     //     setEmailError(t('Err_Yup_Required'))
-    //     // }
-    //
-    //     // if (emailError.length === 0) setEmailError(t('Err_Yup_Required'))
-    //
-    //     if (grecaptcha.getResponse() === '') {
-    //         // event.preventDefault()
-    //         setRobot(true)
-    //         // alert("Please click <I'm not a robot> before sending the job")
-    //     }
-    //
-    //     // Execute the reCAPTCHA when the form is submitted
-    //     // recaptchaRef.current.execute()
-    // }
-
     const onReCAPTCHAChange = (captchaCode: string) => {
-        setRobot(false) // текст с ошибкой
+        // console.log('captchaCode in recaptchaRef', recaptchaRef.current.getValue())
 
-        // If the reCAPTCHA code is null or undefined indicating that
-        // the reCAPTCHA was expired then return early
-        if (!captchaCode) {
-            return
+        if (recaptchaRef.current.getValue()) {
+            setValue('recaptcha', recaptchaRef.current.getValue())
+            setRobot(false)
+            // console.log('onChange recaptcha hook form value', control._getWatch('recaptcha'))
         }
-        // Else reCAPTCHA was executed successfully so proceed with the
-        // alert(`Hey, ${email}`) // пример
-        // setValue('recaptcha', captchaCode)
-
-        // Reset the reCAPTCHA so that it can be executed again if user submits another email
-        recaptchaRef.current.reset()
     }
 
     useErrorSnackbar(isError)
+    if (isLoading) return <LoaderScreen variant={'circle'} />
 
     return (
         <FormWrapper marginTop={96}>
@@ -121,7 +95,6 @@ export const ForgotPasswordForm = () => {
                     open={open}
                     setOpen={setOpen}
                     header={t('EmailSent')}
-                    // text={t('HaveSent') + email}
                     text={t('HaveSent') + control._getWatch('email')}
                     buttonTitleOK={t('MainButton')}
                     extraCallbackOK={() => {
@@ -138,28 +111,16 @@ export const ForgotPasswordForm = () => {
                         control={control}
                         error={errors.email?.message}
                     />
-
-                    {/* <Input */}
-                    {/*    id={'Forgot_Email'} */}
-                    {/*    name={'email'} */}
-                    {/*    type={'email'} */}
-                    {/*    value={email} */}
-                    {/*    onChange={(e) => { */}
-                    {/*        setEmail(e.currentTarget.value) */}
-                    {/*    }} */}
-                    {/*    placeholder={t('Email')} */}
-                    {/*    // error={emailError} */}
-                    {/* /> */}
                     <p>{t('EnterEmail')}</p>
                 </div>
 
                 {isSuccess ? <div>{t('SendLinkSuccess')}</div> : ''}
 
                 <Button
+                    id={'ForgotSendLinkButton'}
                     className={styles.btn}
                     theme={'primary'}
                     type={'submit'}
-                    id={'ForgotSendLinkButton'}
                 >
                     {!isSuccess ? t('SendLink') : t('SendLinkAgain')}
                 </Button>
@@ -167,43 +128,61 @@ export const ForgotPasswordForm = () => {
                     {t('BackToSignIn')}
                 </Link>
 
-                {!isSuccess ? (
-                    <div className={styles.captcha_google}>
-                        {robot ? (
-                            <div className={styles.robot_message}>{t('RecaptchaRobotMessage')}</div>
-                        ) : (
-                            ''
-                        )}
-                        <div className={styles.recaptcha}>
-                            <ReCAPTCHA
-                                ref={recaptchaRef}
-                                // size='invisible'
-                                size='normal'
-                                // sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
-                                // sitekey={process.env.local.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
-                                sitekey={NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
-                                onChange={onReCAPTCHAChange}
-                            />
-                        </div>
+                <div className={styles.captcha_google}>
+                    {robot ? (
+                        <div className={styles.robot_message}>{t('RecaptchaRobotMessage')}</div>
+                    ) : (
+                        ''
+                    )}
+                    <div className={styles.recaptcha}>
+                        <ReCAPTCHA
+                            ref={recaptchaRef}
+                            size='normal'
+                            // sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+                            // sitekey={process.env.local.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+                            sitekey={NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+                            onChange={onReCAPTCHAChange}
+                        />
                     </div>
-                ) : (
-                    // <div className={styles.captcha}>
-                    //      <div className={styles.checkboxBody}>
-                    //         <div>
-                    //             {/* <Controller */}
-                    //             {/*    name='recaptcha' */}
-                    //             {/*    control={control} */}
-                    //             {/*    render={({ field }) => ( */}
-                    //             {/*        <Checkbox {...field} error={errors.recaptcha?.message} /> */}
-                    //             {/*    )} */}
-                    //             {/* /> */}
-                    //         </div>
-                    //         <label htmlFor={'recaptcha'}>{t('Robot')}</label>
-                    //      </div>
-                    //      <CaptchaIcon />
-                    // </div>
-                    ''
-                )}
+                </div>
+
+                {/* {!isSuccess ? ( */}
+                {/*    <div className={styles.captcha_google}> */}
+                {/*        {robot ? ( */}
+                {/*            <div className={styles.robot_message}>{t('RecaptchaRobotMessage')}</div> */}
+                {/*        ) : ( */}
+                {/*            '' */}
+                {/*        )} */}
+                {/*        <div className={styles.recaptcha}> */}
+                {/*            <ReCAPTCHA */}
+                {/*                ref={recaptchaRef} */}
+                {/*                // size='invisible' */}
+                {/*                size='normal' */}
+                {/*                // sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY} */}
+                {/*                // sitekey={process.env.local.NEXT_PUBLIC_RECAPTCHA_SITE_KEY} */}
+                {/*                sitekey={NEXT_PUBLIC_RECAPTCHA_SITE_KEY} */}
+                {/*                onChange={onReCAPTCHAChange} */}
+                {/*            /> */}
+                {/*        </div> */}
+                {/*    </div> */}
+                {/* ) : ( */}
+                {/*    // <div className={styles.captcha}> */}
+                {/*    //      <div className={styles.checkboxBody}> */}
+                {/*    //         <div> */}
+                {/*    //             /!* <Controller *!/ */}
+                {/*    //             /!*    name='recaptcha' *!/ */}
+                {/*    //             /!*    control={control} *!/ */}
+                {/*    //             /!*    render={({ field }) => ( *!/ */}
+                {/*    //             /!*        <Checkbox {...field} error={errors.recaptcha?.message} /> *!/ */}
+                {/*    //             /!*    )} *!/ */}
+                {/*    //             /!* /> *!/ */}
+                {/*    //         </div> */}
+                {/*    //         <label htmlFor={'recaptcha'}>{t('Robot')}</label> */}
+                {/*    //      </div> */}
+                {/*    //      <CaptchaIcon /> */}
+                {/*    // </div> */}
+                {/*    '' */}
+                {/* )} */}
             </form>
         </FormWrapper>
     )
