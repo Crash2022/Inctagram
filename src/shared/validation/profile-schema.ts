a526c5ec9074b18092529f8a852a9111d8aeea7b
Crash2022 <crash555@mail.ru>
2023-06-19 11:55:12 +0500

    add: extra validation to profile data

--- src/shared/validation/profile-schema.ts
+++ src/shared/validation/profile-schema.ts
@@ -1,12 +1,31 @@
 import * as yup from 'yup'
 import { TranslateFn } from '@/shared/types/translate-type'
+import { onlyLettersRegExp, onlyLettersWithDashRegExp } from '@/shared/validation/validationRegExp'
 
 export const ProfileSchema = (t: TranslateFn) => {
     return yup.object().shape({
-        userName: yup.string().min(6, t('Err_Yup_Min_Name')).max(30, t('Err_Yup_Max_Name')),
-        firstName: yup.string().max(50, t('Err_Yup_Max_NameExtra')),
-        lastName: yup.string().max(50, t('Err_Yup_Max_NameExtra')),
-        city: yup.string().max(50, t('Err_Yup_Max_NameExtra')),
+        userName: yup.string().min(6, t('Err_Yup_Min_UserName')).max(30, t('Err_Yup_Max_UserName')),
+        firstName: yup
+            .string()
+            .max(50, t('Err_Yup_Max_Name'))
+            .matches(onlyLettersRegExp, {
+                message: t('Err_Yup_OnlyLetters_Name'),
+                excludeEmptyString: false
+            }),
+        lastName: yup
+            .string()
+            .max(50, t('Err_Yup_Max_Name'))
+            .matches(onlyLettersRegExp, {
+                message: t('Err_Yup_OnlyLetters_Name'),
+                excludeEmptyString: false
+            }),
+        city: yup
+            .string()
+            .max(50, t('Err_Yup_Max_NameExtra'))
+            .matches(onlyLettersWithDashRegExp, {
+                message: t('Err_Yup_OnlyLettersWithDash_City'),
+                excludeEmptyString: false
+            }),
         // dateOfBirth: yup.string().max(50, t('Err_Yup_Max_NameExtra')),
         aboutMe: yup
             .string()
---
+++ src/shared/validation/validationRegExp.ts
@@ -0,0 +1,6 @@
+// только буквы и цифры
+export const onlyLettersDigitsRegExp = /^[а-яА-ЯёЁa-zA-Z0-9]+$/
+// только буквы (без спец. символов)
+export const onlyLettersRegExp = /^[а-яА-ЯёЁa-zA-Z]+$/
+// только буквы и тире (без других спец. символов)
+export const onlyLettersWithDashRegExp = /^[а-яА-ЯёЁa-zA-Z/-]+$/
